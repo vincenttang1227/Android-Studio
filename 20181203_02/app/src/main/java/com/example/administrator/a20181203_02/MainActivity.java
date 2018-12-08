459a4ab887f529f6e9813497dd048a5695d82e59
VincentTang <vincent.tang@msa.hinet.net>
2018-12-08 11:16:42 +0800

    20181208 修正作業檔案

--- 20181203_02/app/src/main/java/com/example/administrator/a20181203_02/MainActivity.java
+++ 20181203_02/app/src/main/java/com/example/administrator/a20181203_02/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     private TextView tvSug;
     private EditText etSex, etAge;
     private Button btnOk;
@@ -23,41 +22,36 @@ public class MainActivity extends AppCompatActivity {
         etAge = findViewById(R.id.etAge);
         btnOk = findViewById(R.id.btnOk);
         tvSug = findViewById(R.id.tvSug);
-
-        btnOk.setOnClickListener(btnOkListener);
     }
 
-    private Button.OnClickListener btnOkListener = new Button.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if (btnOk.getText().toString().equals("確定")) {
-                btnOk.setText("重來");
-                String inputSex = etSex.getText().toString();
-                int inputAge = Integer.parseInt(etAge.getText().toString());
-                String sugStr = getString(R.string.suggest);
+    public void btnOnClick(View view) {
+        if (btnOk.getText().toString().equals("確定")) {
+            btnOk.setText("重來");
+            String inputSex = etSex.getText().toString();
+            int inputAge = Integer.parseInt(etAge.getText().toString());
+            String sugStr = getString(R.string.suggest);
 
-                if (inputSex.equals(getString(R.string.male))) {
-                    if (inputAge < 30)
-                        sugStr += (R.string.notHurry);
-                    else if (inputAge > 35)
-                        sugStr += (R.string.getMarry);
-                    else
-                        sugStr += (R.string.startFinding);
-                } else {
-                    if (inputAge < 27)
-                        sugStr += (R.string.notHurry);
-                    else if (inputAge > 32)
-                        sugStr += (R.string.getMarry);
-                    else
-                        sugStr += (R.string.startFinding);
-                }
-                tvSug.setText(sugStr);
+            if (inputSex.equals(getString(R.string.male))) {
+                if (inputAge < 30)
+                    sugStr += getString(R.string.notHurry);
+                else if (inputAge > 35)
+                    sugStr += getString(R.string.getMarry);
+                else
+                    sugStr += getString(R.string.startFinding);
             } else {
-                btnOk.setText("確定");
-                etSex.setText("");
-                etAge.setText("");
-                tvSug.setText(R.string.suggest);
+                if (inputAge < 27)
+                    sugStr += getString(R.string.notHurry);
+                else if (inputAge > 32)
+                    sugStr += getString(R.string.getMarry);
+                else
+                    sugStr += getString(R.string.startFinding);
             }
+            tvSug.setText(sugStr);
+        } else {
+            btnOk.setText("確定");
+            etSex.setText("");
+            etAge.setText("");
+            tvSug.setText(R.string.suggest);
         }
-    };
+    }
 }
